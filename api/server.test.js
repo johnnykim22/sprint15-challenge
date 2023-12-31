9199408ce7082d7b062c4c93aca05fc789436b86
johnnykim22 <johnnykim22@gmail.com>
2023-12-31 08:15:07 -0800

    api

--- api/server.test.js
+++ api/server.test.js
@@ -4,7 +4,7 @@ test('register',async()=>{
 
   //post to localhost:9000 , (username:test1,password:some password)
   ///response? {username:test1}
-let res1=await axios.post("http://localhost:9000/api/auth/register",
+let res1=await axios.post("/api/auth/register",
 {
   "username":"test13",
   "password":"12345"
@@ -20,7 +20,7 @@ test('Not allow duplicate user name',async()=>{
 
   //post to localhost:9000 , (username:test1,password:some password)
   ///response? {username:test1}
-let res1=await axios.post("http://localhost:9000/api/auth/register",
+let res1=await axios.post("/api/auth/register",
 {
   "username":"test13",
   "password":"12345"
