3baf256caf638814ac61fd2a1fa71903407e6297
johnnykim22 <johnnykim22@gmail.com>
2023-12-31 17:57:57 -0800

    testjs

--- api/server.test.js
+++ api/server.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const db = require('../data/dbConfig')
+const db = require('../data/dbConfig.js')
 const server = require('./server.js')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
