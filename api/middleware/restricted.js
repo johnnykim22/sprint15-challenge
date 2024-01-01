24b8b36bd1f7fa935ce280f70d618f70580425c5
johnnykim22 <johnnykim22@gmail.com>
2023-12-31 18:02:57 -0800

    token

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -4,11 +4,11 @@ const secret = process.env.JWT_SECRET || "shh"; // Consistency with the secret u
 
 function restricted(req, res, next) {
   const token = req.headers.authorization;
-  console.log(req.headers);
-  console.log(token);
+console.log(req.headers);
+console.log(token);
   if (token) {
     console.log('testing');
-    jwt.verify(token, secret, (err, decodedToken) => {  // Corrected line
+    jwt.verify(token, , (err, decodedToken) => {
       console.log(err);
       if (err) {
         res.status(401).json({ message: "token invalid" });
@@ -19,8 +19,9 @@ function restricted(req, res, next) {
     });
   } else {
     console.log('HERE');
-    res.status(401).json({ message: "no token provided HERE" });
+    res.status(401).json({ message: "token required" });
   }
 }
 
 module.exports = restricted;
+
